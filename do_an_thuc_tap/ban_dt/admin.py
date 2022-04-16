6129e36cdaf1469e8544f1cb64462037f31db634
Tran-Quoc-Tuan <tranquoct700@gmail.com>
2022-04-16 14:53:29 +0700

    Update admin page

--- do_an_thuc_tap/ban_dt/admin.py
+++ do_an_thuc_tap/ban_dt/admin.py
@@ -1,19 +1,66 @@
-from re import S
-from django.contrib import admin
+from django.contrib import admin, messages
 from .models import (
     DongSP, SanPham, CauHinh, Mau, Gia,
     ThuongHieu, ChiTietGioHang, HoaDon, ChiTietHoaDon,
-    BinhLuan, PhanHoi, Anh
+    BinhLuan, PhanHoi, Anh, Trạng_thái_sản_phẩm
 )
-admin.site.register(DongSP)
-admin.site.register(SanPham)
-admin.site.register(CauHinh)
-admin.site.register(Mau)
-admin.site.register(Gia)
-admin.site.register(ThuongHieu)
-admin.site.register(ChiTietGioHang)
-admin.site.register(HoaDon)
-admin.site.register(ChiTietHoaDon)
-admin.site.register(BinhLuan)
-admin.site.register(PhanHoi)
+
+
+class CauHinhAdmin(admin.TabularInline):
+    model = CauHinh
+
+class MauAdmin(admin.TabularInline):
+    model = Mau
+
+@admin.register(SanPham)
+class SanPhamAdmin(admin.ModelAdmin):
+    actions = ['khoaSanPham', 'moSanPham']
+    search_fields = ['ten', 'ThuongHieu__ten', 'dongSP__dongSP', 'nam']
+    inlines = [MauAdmin, CauHinhAdmin]
+
+    @admin.action(description='Khóa sản phẩm')
+    def khoaSanPham(self, request, sanPham):
+        updated = sanPham.update(trangThai = Trạng_thái_sản_phẩm.KHÓA)
+        self.message_user(request, '%d sản phẩm đã bị khóa!' % updated, messages.WARNING)
+
+    @admin.action(description='Mở sản phẩm')
+    def moSanPham(self, request, sanPham):
+        updated = sanPham.update(trangThai = Trạng_thái_sản_phẩm.MỞ)
+        self.message_user(request, '%d sản phẩm đã được mở!' % updated, messages.SUCCESS)
+
+
+@admin.register(Gia)
+class GiaSanPham(admin.ModelAdmin):
+    search_fields = ['sanPham__ten']
+
+class PhanHoiAdmin(admin.TabularInline):
+    model = PhanHoi
+
+@admin.register(BinhLuan)
+class BinhLuanAdmin(admin.ModelAdmin):
+    inlines = [PhanHoiAdmin]
+    search_fields = ['user__name', 'soDT', 'sanPham__ten', 'ngayDang']
+
+
+class ChiTietHDAdmin(admin.TabularInline):
+    model = ChiTietHoaDon
+
+@admin.register(HoaDon)
+class HoaDonAdmin(admin.ModelAdmin):
+    inlines = [ChiTietHDAdmin]
+    search_fields = ['user__name', 'tongTien', 'trangThai']
+
+
+class DongSPAdmin(admin.TabularInline):
+    model = DongSP
+
+@admin.register(ThuongHieu)
+class ThuongHieuAdmin(admin.ModelAdmin):
+    inlines = [DongSPAdmin]
+    search_fields = ['ten']
+
+@admin.register(ChiTietGioHang)
+class ChiTietGHAdmin(admin.ModelAdmin):
+    search_fields = ['user__name', 'sanPham__ten']
+
 admin.site.register(Anh)
