e28df96401ab651c59105a574de53ae9bcb48bfd
tran-quoc-tuan <tranquoct700@gmail.com>
2022-04-17 16:52:48 +0000

    Change from pythonanywhere server

--- do_an_thuc_tap/settings.py
+++ do_an_thuc_tap/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -25,7 +26,7 @@ SECRET_KEY = 'django-insecure-la(wg4g0!if_f^sdkjht#gbnj!o-1&#9=01e_mf0wu5dhv(_de
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost', '192.168.52.103', '127.0.0.1']
+ALLOWED_HOSTS = ['localhost', '192.168.52.103', '127.0.0.1', 'vnquoctuan.pythonanywhere.com']
 
 
 # Application definition
@@ -104,7 +105,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.0/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'vi'
 
 TIME_ZONE = 'UTC'
 
@@ -117,6 +118,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
 MEDIA_URL = 'media/'
 MEDIA_ROOT = ''
